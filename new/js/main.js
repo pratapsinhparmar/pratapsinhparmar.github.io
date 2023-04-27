3cbb4eb28e416ec4d162260699d67888068921c6
Pratapsinh Parmar <pratapsinh.work@gmail.com>
2023-04-27 12:00:44 +0530

    update

--- new/js/main.js
+++ new/js/main.js
@@ -91,3 +91,27 @@ $(document).ready(function () {
     });
 
 });
+
+
+$(document).ready(function(){
+
+    var list = $(".list li");
+    var numToShow = 3;
+    var button = $("#next");
+    var numInList = list.length;
+    list.hide();
+    if (numInList > numToShow) {
+      button.show();
+    }
+    list.slice(0, numToShow).show();
+
+    button.click(function(){
+        var showing = list.filter(':visible').length;
+        list.slice(showing - 1, showing + numToShow).fadeIn();
+        var nowShowing = list.filter(':visible').length;
+        if (nowShowing >= numInList) {
+          button.hide();
+        }
+    });
+
+});

92c3de9f147cde56ff441d06712e53ce6ad748f6
pratapsinhparmar <pratapsinh.work@gmail.com>
2023-12-28 23:15:35 +0530

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,75 @@
-var button = document.querySelector('.fabTrigger');
-var wrap = document.querySelector('.fabWidget');
-// var overlay = document.querySelector('.fabOverlay');
+$(window).on("load", function() {
+    $(".backdrop").addClass("d-none");
+});
 
-button.onclick = function() {
-    button.classList.toggle('active');
-    wrap.classList.toggle('active');
-    // overlay.classList.toggle('active');
-}
+$(document).ready(function() {
 
-// overlay.onclick = function() {
-//     button.classList.toggle('active');
-//     wrap.classList.toggle('active');
-//     overlay.classList.toggle('active');
-// }
+    // hedding top fixed query
+    if ($(window).scrollTop() >= 5) {
+        $(".navbar").addClass("fixed-top");
+    }
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= 5) {
+            $(".navbar").addClass("fixed-top");
+        }
+        else {
+            $(".navbar").removeClass("fixed-top");
+        }
+    });
+
+    // banner text animation query 
+    if($(".my-position").hasClass("test-animation")) {
+        var typing=new Typed(".test-animation", {
+            strings: ["Web Designer", "Web Developer", "Wordpress Developer", "Freelancer"],
+            typeSpeed: 100,
+            backSpeed: 40,
+            loop: true,
+        });
+    }
+
+    // portfolio section query
+    $('.portfolio-slider').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1000,
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ]        
+    });
+
+    // testimonial section start
+    $('.testimonial-slider').slick({
+        arrows: true,
+        centerPadding: "0px",
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        slidesToShow: 1,
+        infinite: true,
+        prevArrow: $('.prev-testi'),
+        nextArrow: $('.next-testi')
+    });
+
+});
+
+
+
+
+
+
+
